0ff2fe29205955c23a166077372a73f9c5ce7238
Jorge <13945741+jorgeblanez@users.noreply.github.com>
2023-05-16 15:55:41 -0300

    Create and print secret
    This commit creates two class methods to define the initial secret and
    to print the current secret. The print method will be useful for when
    we start making guesses, since it will be able to print the right
    guesses as well

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,15 +1,31 @@
 class Hangman
-  attr_reader :dictionary,:word
-  def initialize
+  attr_reader :dictionary,:word, :secret
+
+  def load_dictionary
+    #Load dictionary from file into an array
     @dictionary = File.readlines("google-10000-english-no-swears.txt")
   end
   
   #Get a random word from the dictionary between 5 and 12 characters
   def get_random_word
-    @word = @dictionary.sample.chomp
+    @word = @dictionary.sample.chomp.chars
     while @word.length < 5 || word.length >12
-      @word = @dictionary.sample.chomp
+      @word = @dictionary.sample.chomp.chars
     end
+  end
+
+  def create_secret
+    @secret = Array.new(@word.length)
+    @secret.map!{|value| value = "_"}
+  end
+
+  def print_secret
+    self.secret.each{|value| print value +" "}
+    puts
+  end
+
+  def game
+    @guess = self.create_secret
   end
 
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,8 @@
 require_relative "hangman.rb"
 
 game = Hangman.new
-p game.dictionary
+game.load_dictionary
 game.get_random_word
 p game.word
+game.create_secret
+game.print_secret
